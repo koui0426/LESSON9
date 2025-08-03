46ef46fd6a15001c3da43ca09e6ec4627c191cce
伊藤向香 <xiangxiang0058kf@gmail.com>
2025-08-03 12:01:18 +0900

    ユーザー確認情報ページ

--- main.py
+++ main.py
@@ -5,9 +5,28 @@ st.title("ユーザー情報入力")
 if 'user_name' not in st.session_state:
     st.session_state.user_name = ""
     
+if 'user_grade' not in st.session_state:
+    st.session_state.user_grade = ""
+    
+if 'user_hobby' not in st.session_state:
+    st.session_state.user_hobby = ""
+    
 name = st.text_input("あなたの名前を入力してください")
 if st.button("名前を保存"):
     st.session_state.user_name = name
     st.success("名前を保存しました！")
     
 st.write(f"現在保存されている名前：{st.session_state.user_name}")
+
+
+
+name = st.text_input("名前")
+grade = st.selectbox("学年",["","小学５年","小学６年","中学１年","中学２年","中学３年"])
+hobbies = st.multiselect("趣味",["読書","スポーツ","ゲーム","音楽","絵画","その他"])
+
+if st.button("情報を保存"):
+    st.session_state.user_name = name
+    st.session_state.user_grade = grade
+    st.session_state.user_hobby = hobbies
+    
+    st.success("情報を保存しました！")
--- pages/page1.py
+++ pages/page1.py
@@ -2,9 +2,23 @@ import streamlit as st
 
 st.title("ユーザー情報表示ページ")
 if 'user_name' in st.session_state and st.session_state.user_name:
-    st.success(f"こんにちは、{st.session_state.user_name}さん！")
-    st.write("メインページで入力された名前が正しく表示されています。")
+    st.success("保存されている情報：")
     
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.metric("名前",st.session_state.user_name)
+        st.metric("学年",st.session_state.user_grade)
+        
+    with col2:
+        if st.session_state.get('user_hobby'):
+            st.write("趣味")
+            for hobby in st.session_state.user_hobby:
+                st.write(f"{hobby}")
+        else:
+            st.write("☆☆趣味：☆☆未設定") 
+                
+                   
     st.balloons()
     
 else:
