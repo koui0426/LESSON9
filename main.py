0a95f66ef90b299fb3aab29c31d1e032721642a8
伊藤向香 <xiangxiang0058kf@gmail.com>
2025-08-03 10:24:38 +0900

    名前記憶アプリ

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello word")
+import streamlit as st 
+
+st.title("名前記憶アプリ")
+if'name'not in st.session_state:
+    st.session_state.name = "value"
+    
+name = st.text_input("あなたの名前を入力してください")
+if st.button("名前を記憶"):
+    st.session_state.value = st.session_state.name
